cabf46e09932907620d8ccfb1df1bc31eb8b042b
1243428 <a1243428@01HW849271.local>
2017-11-28 20:21:29 +0530

    testing using karma

---
+++ app/services/users/users.spec.js
@@ -0,0 +1,7 @@
+describe('Users factory', function() {
+
+	it('has a dummy spec to test 2 + 2', function() {
+    // An intentionally failing test. No code within expect() will never equal 4.
+    expect(2+2).toEqual(4);
+  });
+});
